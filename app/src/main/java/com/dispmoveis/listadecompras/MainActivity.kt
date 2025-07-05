94d44d17c17a8ba3fc2f8fffeaa6d093caef22e6
João Rosivaldo Gomes <joaorgomesoficial@gmail.com>
2025-07-05 12:22:57 -0300

    Update(resolvendo posicionamento tela principal )

--- app/src/main/java/com/dispmoveis/listadecompras/MainActivity.kt
+++ app/src/main/java/com/dispmoveis/listadecompras/MainActivity.kt
@@ -74,11 +74,11 @@ class MainActivity : AppCompatActivity(),
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+        /*ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
 
         // --- Configuração da Toolbar (existente) ---
         setSupportActionBar(binding.toolbar)
